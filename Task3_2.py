b42556a4e0c5d743f723989e5a1d38c522f18b67
Yaroslav85Moroz <yarmor85@gmail.com>
2023-04-05 20:01:26 +0300

    Task3

---
+++ Task3_2.py
@@ -0,0 +1,8 @@
+num1 = float(input('Введите число под номером 1: '))
+num2 = float(input('Введите число под номером 2: '))
+num3 = float(input('Введите число под номером 3: '))
+print(round((num1 + num2 + num3)/3, 3))
+
+
+
+
